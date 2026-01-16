36d5827e46dc84fc01edce87af9c8a329cd2339e
Rohit Lavate <rohitlavate97@gmail.com>
2026-01-17 01:43:36 +0530

    Modified BaseTest code

--- src/main/java/com/alchemist/generic/BaseTest.java
+++ src/main/java/com/alchemist/generic/BaseTest.java
@@ -1,9 +1,16 @@
 package com.alchemist.generic;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
+import java.util.Date;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public class BaseTest implements IAutoConstant{
@@ -21,5 +28,24 @@ public class BaseTest implements IAutoConstant{
 		String implicitTimeOut = Lib.getProperty(CONFIG_PATH, "ImplicitTimeOut");
 		int timeOutPeriod = Integer.parseInt(implicitTimeOut);
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(timeOutPeriod));
+	}
+	
+	@AfterMethod
+	public void closeApplication() {
+		driver.close();
+	}
+	
+	public void takeScreenshot(String testname) {
+		Date date = new Date();
+		String currentDate = date.toString().replaceAll(":", "_");
+		TakesScreenshot ts = (TakesScreenshot)driver;
+		File src = ts.getScreenshotAs(OutputType.FILE);
+		File dest = new File(".\\screenshots\\"+currentDate+"\\"+testname+"_screenshot.png");
+		try {
+			FileUtils.copyFile(src, dest);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }

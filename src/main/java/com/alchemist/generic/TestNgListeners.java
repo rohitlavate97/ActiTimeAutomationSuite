1108cca22f6d28a68e96f6dfaca64aa3da6514b3
Rohit Lavate <rohitlavate97@gmail.com>
2026-01-17 01:50:51 +0530

    Added Listener class

---
+++ src/main/java/com/alchemist/generic/TestNgListeners.java
@@ -0,0 +1,59 @@
+package com.alchemist.generic;
+
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class TestNgListeners implements ITestListener{
+	BaseTest b = new BaseTest();
+
+	@Override
+	public void onTestStart(ITestResult result) {
+		// TODO Auto-generated method stub
+		ITestListener.super.onTestStart(result);
+	}
+
+	@Override
+	public void onTestSuccess(ITestResult result) {
+		// TODO Auto-generated method stub
+		ITestListener.super.onTestSuccess(result);
+	}
+
+	@Override
+	public void onTestFailure(ITestResult result) {
+	    System.out.println("Test Failed: " + result.getName());
+	    b.takeScreenshot(result.getName()); 
+	    // No need for ITestListener.super.onTestFailure(result);
+	}
+
+	@Override
+	public void onTestSkipped(ITestResult result) {
+		// TODO Auto-generated method stub
+		ITestListener.super.onTestSkipped(result);
+	}
+
+	@Override
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+		// TODO Auto-generated method stub
+		ITestListener.super.onTestFailedButWithinSuccessPercentage(result);
+	}
+
+	@Override
+	public void onTestFailedWithTimeout(ITestResult result) {
+		// TODO Auto-generated method stub
+		ITestListener.super.onTestFailedWithTimeout(result);
+	}
+
+	@Override
+	public void onStart(ITestContext context) {
+		// TODO Auto-generated method stub
+		ITestListener.super.onStart(context);
+	}
+
+	@Override
+	public void onFinish(ITestContext context) {
+		// TODO Auto-generated method stub
+		ITestListener.super.onFinish(context);
+	}
+	
+}
